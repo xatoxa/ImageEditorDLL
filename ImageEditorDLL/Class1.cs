1a4ea680d084bf14fea6bba1d38bd9d98dd03161
Anton Hatuncev <anton.xatuncev@mail.ru>
2019-01-31 13:33:48 +0300

    скелет класса, нужно изменить свойства проекта

--- ImageEditorDLL/Class1.cs
+++ ImageEditorDLL/Class1.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ImageEditorDLL
 {
-    public class Class1
+    
+    [Guid("5B8AC727-DA4E-430F-B23E-B5EB5D01565A")]
+    internal interface IMyClass
     {
+        [DispId(1)]
+
+        //методы, которые будут вызываться извне
+        string printin1C(string msg);
+
     }
+
+
+    [Guid("E101E314-0281-4665-8FAB-64408C4C638B"),
+    InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IMyEvents
+    {
+
+    }
+
+
+    [Guid("9AD67B75-A434-41DB-9B2C-C6E81E815B77"), ClassInterface(ClassInterfaceType.None),
+        ComSourceInterfaces(typeof(IMyEvents))]
+    public class MyClass : IMyClass
+    {
+        public string printin1C(string msg)
+        {
+            return msg;
+        }
+    }
+
+
+
 }
