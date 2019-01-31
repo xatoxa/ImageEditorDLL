08539d68cc72ebd6fa2537bd08498ffd6b86d62c
Anton Hatuncev <anton.xatuncev@mail.ru>
2019-01-31 17:44:15 +0300

    написаны заголовки функций, свойства - готово

--- ImageEditorDLL/Class1.cs
+++ ImageEditorDLL/Class1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
@@ -14,8 +15,12 @@ namespace ImageEditorDLL
         [DispId(1)]
 
         //методы, которые будут вызываться извне
-
+        //print - просто пример, потом её надо будет удалить
         string printin1C(string msg);
+
+        Image Cut_image(Image image, uint x1, uint y1, uint x2, uint y2);
+
+        Image Insert_image(Image image1, uint x1, uint y1, Image image2);
 
     }
 
@@ -37,6 +42,19 @@ namespace ImageEditorDLL
         public string printin1C(string msg)
         {
             return msg;
+        }
+
+        public Image Cut_image(Image image, uint x1, uint y1, uint x2, uint y2)
+        {
+
+            return null; //заглушка, должна возвратить Image;
+        }
+
+        public Image Insert_image(Image image1, uint x1, uint y1, Image image2)
+        {
+
+
+            return null;
         }
     }
 
