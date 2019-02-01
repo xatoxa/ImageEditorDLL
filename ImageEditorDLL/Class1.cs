0de981317d83b0b6c5171a4dff1ee37ab2b85bf4
Anton Hatuncev <anton.xatuncev@mail.ru>
2019-02-01 13:36:10 +0300

    Release

--- ImageEditorDLL/Class1.cs
+++ ImageEditorDLL/Class1.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace ImageEditorDLL
 {
@@ -39,6 +40,33 @@ namespace ImageEditorDLL
     
         public Image Cut_image(Image image, uint x1, uint y1, uint x2, uint y2)
         {
+            if (image == null)
+            {
+                MessageBox.Show("image not found;\nimage == NULL", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (x2 > image.Width)
+            {
+                MessageBox.Show("x2 exceeds the width of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (y2 > image.Height)
+            {
+                MessageBox.Show("y2 exceeds the height of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (x1 > (image.Width - x2))
+            {
+                MessageBox.Show("x1 exceeds the new width of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (y1 > (image.Height - y2))
+            {
+                MessageBox.Show("y1 exceeds the new height of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+
             Bitmap bmp = image as Bitmap;
 
             Bitmap result = bmp.Clone(new Rectangle((int)x1, (int)y1, (int)x2, (int)y2),bmp.PixelFormat);
@@ -50,7 +78,26 @@ namespace ImageEditorDLL
 
         public Image Insert_image(Image image1, uint x1, uint y1, Image image2)
         {
-            if (image1 == null || image2 == null) return null; 
+            if (image1 == null)
+            {
+                MessageBox.Show("image1 not found;\nimage1 == NULL", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (image2 == null)
+            {
+                MessageBox.Show("image 2 not found;\nimage2 == NULL", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (x1 > image1.Width)
+            {
+                MessageBox.Show("x exceeds the width of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+            if (y1 > image1.Height)
+            {
+                MessageBox.Show("y exceeds the height of image1", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
 
 
             Bitmap result = new Bitmap(image1.Width, image1.Height);
